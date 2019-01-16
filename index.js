21b7af4aecfbe62d78397356f579691b8c329a1e
Shannon Code <genecyber@gmail.com>
2019-01-16 14:50:53 -0500

    cleanup of feed handling

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ function init(opts, eventHooks){
     var _opts = {
         mySubscribeKey: "sub-c-95c943a2-5962-11e4-9632-02ee2ddab7fe", 
         myPublishKey: "pub-c-a281bc74-72b6-4976-88ec-e039492b0dfa",
-        myChannel: "dat_archival"
+        myChannel: "dat_archival",
+        /* feedPath: path.resolve(__dirname, 'feeds') */
     }
     var _eventHooks = {
         read: readArchivistFlatFeed, 
@@ -63,13 +64,18 @@ function readArchivistFlatFeed(type, cb){
     if (!cb) return readArchivistFlatFeed(type, ()=>{})
     
     var feedSrc = path.resolve(__dirname, 'feeds')
-    var archivistFeed = fs.readFileSync(feedSrc).toString().split(os.EOL)
-    archivistFeed.forEach((item, index)=>{
-        if (!LiveFeeds.shards.includes(item)) LiveFeeds.shards.push(item)
-        if (index+1 === archivistFeed.length) {
-            return cb(LiveFeeds)
-        }
-    })
+    var feedData = fs.readFileSync(feedSrc).toString()
+    if(feedData) {        
+        var archivistFeed = fs.readFileSync(feedSrc).toString().trim().split(os.EOL)
+        archivistFeed.forEach((item, index)=>{
+            if (!LiveFeeds.shards.includes(item)) LiveFeeds.shards.push(item)
+            if (index+1 === archivistFeed.length) {
+                return cb(LiveFeeds)
+            }
+        })
+    } else {
+        return cb(LiveFeeds)
+    }
 }
 
 function saveMetadata(type, keyHex, content){
@@ -101,19 +107,25 @@ function saveArchivistFeed(type, keyHex, cb){
         if (!feedData.includes(keyHex)) feedData.push(keyHex)
         if (!feeds.LiveFeeds.shards.includes(keyHex)) feeds.LiveFeeds.shards.push(keyHex)
     }
-    feeds.LiveFeeds.shards.forEach((shard, index)=>{
-        if (!feedData.includes(shard)) feedData.push(shard)
-        if (index+1 === feeds.LiveFeeds.shards.length) {
-            fs.writeFileSync(feedJsonSrc, JSON.stringify(feeds.LiveFeeds, null, 4))
-            var feedContent = fs.readFileSync(feedSrc).toString()
-            var isSame = (feedContent === feedData.join(os.EOL))
-            if (!isSame) {
-                fs.writeFile(feedSrc, feedData.join(os.EOL), err=>{
+    if (feeds.LiveFeeds.shards.length > 0) {
+        feeds.LiveFeeds.shards.forEach((shard, index)=>{
+            if (!feedData.includes(shard)) feedData.push(shard)
+            if (index+1 === feeds.LiveFeeds.shards.length) {
+                fs.writeFileSync(feedJsonSrc, JSON.stringify(feeds.LiveFeeds, null, 4))
+                var feedContent = fs.readFileSync(feedSrc).toString()
+                var isSame = (feedContent === feedData.join(os.EOL))
+                if (!isSame) {
+                    fs.writeFile(feedSrc, feedData.join(os.EOL), err=>{
+                        return cb(feedData)
+                    })
+                } else {
                     return cb(feedData)
-                })
-            } else {
-                return cb(feedData)
-            }         
-        }
-    })
+                }         
+            }
+        })
+    } else {
+        console.log("Last one?")
+        fs.writeFileSync(feedJsonSrc, JSON.stringify(feeds.LiveFeeds, null, 4))
+        return cb(feedData)
+    }
 }

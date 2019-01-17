1ea610ff91fd2dd4eb30b66099452318dfed8f62
Shannon Code <genecyber@gmail.com>
2019-01-17 18:45:39 -0500

    archivists register with each other and sign each other pubkeys

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+var Coval = require('coval.js')
+var Message = require('bitcore-message')
+var fp = require("find-free-port")
+var Hdkey = new Coval.Coval().Secure.HDKey
+var hdkey = new Hdkey()
 var HypercoreDaemon = require('hypercored')
 var fs = require('fs-extra')
 var path = require('path')
@@ -6,8 +11,11 @@ var LiveFeeds = {shards: [], metadata: []}
 var WatchJS = require("melanke-watchjs")
 var runningAsScript = !module.parent
 var express = require('express')
-
+var PubNub = require('pubnub')
+var secretsContent, pubnub, pubnubOptions, bitcore
+var isRegistered = false
 var app = express()
+var secretsPath = path.resolve(__dirname, ".secrets")
 app.set('json spaces', 4)
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
@@ -15,6 +23,178 @@ app.use(function (req, res, next) {
     next()
 })
 
+function preInit(){
+    
+    // check for file on system
+    // if no file exists create one
+    //      generate hdkey and store locally
+    //      broadcast pubkey + address and intent to register
+    //      each archivist online can receive the request and sign the pubkey with their private key
+    //      each archivist broadcasts signed key back to registrant
+    //      each archivist stores known peers
+
+    secretsContent = initSecrets()
+    isRegistered = initRegistered(secretsContent)
+    console.log("Your Archivist Address is", secretsContent.address)
+    console.log("Registered?", isRegistered)
+}
+
+function initSecrets(){
+    bitcore = hdkey.GetBitcore()
+    fs.ensureFileSync(secretsPath)
+    secretsContent = readSecrets()
+    if (secretsContent.length === 0) {        
+        hdkey.StandardHDKey('0', function(address, key){
+            secretsContent = {address: address, key: key}
+            saveSecrets(secretsContent)
+        })        
+    } else {
+        secretsContent = JSON.parse(secretsContent)
+        secretsContent.key = bitcore.HDPrivateKey(secretsContent.key)
+    }
+    return secretsContent
+}
+
+function readSecrets(){
+    return fs.readFileSync(secretsPath)
+}
+function saveSecrets(secretsContent){
+    fs.writeFileSync(secretsPath, JSON.stringify(secretsContent, null, 4))
+}
+
+function initRegistered(secretsContent){
+    if (secretsContent.peers && secretsContent.peers.length > 10) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function startRegistration(){
+    var payload = {
+        type: "register",
+        address: secretsContent.address,
+        pubkey: secretsContent.key.publicKey.toString('hex')
+    }
+    pubnub.publish({ 
+            message: payload,
+            channel: 'dat_archival'
+        }, 
+        function (status) {
+            //console.log("msg status", status)
+        }
+    )
+}
+
+function initPubNub(opts){
+    pubnubOptions = opts
+    pubnub = new PubNub({
+        subscribeKey: opts.mySubscribeKey || 'demo',
+        publishKey: opts.myPublishKey || 'demo',
+        secretKey: opts.secretKey || '',
+        ssl: true
+    })
+    pubnub.addListener({
+        status: function (statusEvent) {
+            
+        },
+        message: function (envelope) {
+            var payload = envelope.message
+            if (payload.type === "register") {
+                handleRegisterMsg(payload)
+            } else if(payload.type === "register-sign") {
+                handleSignedRegisterMsg(payload)
+            }
+            
+        },
+        presence: function (presenceEvent) {
+            
+        }
+    })
+    pubnub.subscribe({
+        channels: [pubnubOptions.myChannel || 'demo'],
+    })
+    return pubnub
+}
+
+function handleSignedRegisterMsg(payload){
+    if (!secretsContent.peers) secretsContent.peers = []
+    payload.signer.signature = payload.signature
+    var forMe = payload.registrant.address === secretsContent.address && secretsContent.peers.filter(peer=>{return peer.signature && peer.signature === payload.signer.signature}).length < 1
+    if (forMe) {
+        console.log("EPIC someone signed my pubkey", payload)
+        var validSignature = verifySignature(secretsContent.key.publicKey.toString('hex'), payload.signature, payload.signer.publicKey)
+        if (validSignature) {
+            var foundPeer = secretsContent.peers.filter(peer=>{return peer.address === payload.signer.address})
+            if (foundPeer.length > 0) {
+                foundPeer[0].signature = payload.signer.signature
+            } else {
+                secretsContent.peers.push(payload.signer)
+            }
+            saveSecrets(secretsContent)
+            console.log("Saved that signer to my secrets")
+        }
+    } else {
+        console.log("Heard my signed registration response")
+    }
+}
+
+function handleRegisterMsg(payload){
+    var isMe = payload.address === secretsContent.address
+    if (!isMe) {
+        console.log("NEAT someone who isn't me is registering", payload.address, payload.pubkey)
+        if (!secretsContent.peers) secretsContent.peers = []
+        var foundPeer = secretsContent.peers.filter(peer=>{return peer.address === payload.address})
+        if (foundPeer.length < 1) {
+            secretsContent.peers.push({address: payload.address, publicKey: payload.pubkey})
+            saveSecrets(secretsContent)
+        }
+        var signature = signString(payload.pubkey, secretsContent.key.privateKey)
+        var payload = {
+            type: "register-sign",
+            registrant: {
+                address: payload.address,
+                publicKey: payload.pubkey
+            },
+            signer: {
+                address: secretsContent.address, 
+                publicKey: secretsContent.key.publicKey.toString('hex')
+            },
+            signature: signature.toString('hex')
+        }
+        publishToChannel(payload, status=>{
+            if (secretsContent.peers.filter(peer=>{return !peer.signature}).length > 0) {
+                startRegistration()
+            }
+        })
+        
+    } else {
+        console.log("Heard my registration request")
+    }
+}
+
+
+function publishToChannel(payload, cb){
+    pubnub.publish({ 
+        message: payload,
+        channel: 'dat_archival'
+    }, 
+    function (status) {
+        return cb(status)
+    })
+}
+
+function signString(msg, pk){
+    signature = bitcore.crypto.ECDSA.sign(bitcore.crypto.Hash.sha256(new Buffer(msg)), pk)
+    return signature
+}
+
+function verifySignature(msg, sig, pubkey){
+    var verification = bitcore.crypto.ECDSA.verify(bitcore.crypto.Hash.sha256(new Buffer(msg)), bitcore.crypto.Signature.fromString(sig), bitcore.PublicKey.fromString(pubkey))
+    return verification
+}
+
+preInit()
 
 function Feeds(liveFeeds){
     this.LiveFeeds = liveFeeds
@@ -66,13 +246,11 @@ app.post('/remove', (req, res)=>{
             }
         })
     })
-
-    //var newShards = feeds.LiveFeeds.shards.filter(shard=>{return !toRemove.includes(shard)})
-    //feeds.LiveFeeds.shards = newShards
-    
-    
 })
-app.listen(3001)
+fp(3000, function(err, freePort){
+    app.listen(freePort)
+    console.log('listening on port', freePort)
+})
 
 function init(opts, eventHooks){
     var _opts = {
@@ -93,11 +271,16 @@ function init(opts, eventHooks){
     fs.ensureFileSync(feedJsonFilePath)
     
     HypercoreDaemon.init(eventHooks || _eventHooks, opts || _opts)
-    readArchivistFlatFeed('open', (liveFeeds) => {
-        feeds.watch('shards', (id, oldval, newval) => {
-            console.log(id, oldval, newval)
+    pubnub = initPubNub(_opts)
+    if (!isRegistered) {
+        return startRegistration()
+    } else {
+        readArchivistFlatFeed('open', (liveFeeds) => {
+            feeds.watch('shards', (id, oldval, newval) => {
+                console.log(id, oldval, newval)
+            })
         })
-    })
+    }
 }
 
 
@@ -109,8 +292,6 @@ module.exports = {
 if (runningAsScript) {
     init()
 }
-
-
 
 function readArchivistFlatFeed(type, cb){
     if (!cb) return readArchivistFlatFeed(type, ()=>{})
@@ -179,6 +360,6 @@ function saveArchivistFeed(type, keyHex, cb){
         fs.writeFile(feedSrc, feedData.join(os.EOL), err=>{
             fs.writeFileSync(feedJsonSrc, JSON.stringify(feeds.LiveFeeds, null, 4))
             return cb(feedData)
-        })        
+        })
     }
 }
